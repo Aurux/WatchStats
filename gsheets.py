27e4b4310700ef7896f7df5e350f613f46fdffaa
Ben Wilson <benjamin.wilson2@icloud.com>
2022-09-16 19:51:04 +0100

    Setup API call

--- gsheets.py
+++ gsheets.py
@@ -1,2 +1,13 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+# Setup API connection
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+
+client = gspread.authorize(creds)
+
+sheet = client.open("Copy of Comparison: Most Popular Mobile OS")
+worksheet = sheet.worksheet("Researcher Book")
